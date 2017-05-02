c54a8094753d6c46ad13fb4fce2d5ba4bd16ba23
shmilee <shmilee.zju@gmail.com>
2017-05-02 17:04:40 +0800

    gdr: readraw, use npz, or hdf5 as you wish

--- gdpy3/read/readraw.py
+++ gdpy3/read/readraw.py
@@ -17,15 +17,18 @@ log = logging.getLogger('gdr')
 
 class ReadRaw(ReadNpz):
     '''Read all GTC .out files in directory ``datadir``.
+
     Convert them to a .npz file saved in ``datadir``.
     Then this cls behaves as ``read.ReadNpz``.
+    If the extension of saved file is given as .npz or .hdf5,
+    they will be converted to a .[extension] file.
 
     Attributes
     ----------
     datadir: str
         path of GTC .out files
     file: str
-        path of .npz file
+        path of .npz, .hdf5 file
     datakeys: tuple
         keys of physical quantities in the .out files
     desc: str
@@ -37,8 +40,10 @@ class ReadRaw(ReadNpz):
     datadir: str
         the GTC .out files to open
     kwargs: other parameters
-        ``description``, ``version``, ``additionalpats`` for gdc.convert
-        ``salt`` for name of saved .npz file, default 'gtc.out'
+        ``loglevel``, ``description``,
+        ``version``, ``additionalpats`` for gdc.convert
+        ``salt`` for name of saved file, default 'gtc.out'
+        ``extension`` for extension of saved file, default 'npz'
 
     Examples
     --------
@@ -55,35 +60,57 @@ class ReadRaw(ReadNpz):
 
         # salt
         if 'salt' in kwargs:
-            salt = str(kwargs['salt'])
+            salt = os.path.join(datadir, str(kwargs['salt']))
+            if not os.path.isfile(salt):
+                log.error("Can't find file '%s'!" % salt)
+            else:
+                salt = os.path.join(datadir, 'gtc.out')
         else:
-            salt = 'gtc.out'
-        if not os.path.isfile(datadir + '/' + salt):
-            raise IOError("Can't find '%s' in '%s'!" % (salt, datadir))
+            salt = os.path.join(datadir, 'gtc.out')
+        log.debug("Use file %s as salt." % salt)
+
         try:
-            with open(datadir + '/' + salt, 'r') as f:
+            with open(salt, 'r') as f:
                 salt = sha1(f.read().encode('utf-8')).hexdigest()
+                log.debug("Get salt string: '%s'." % salt)
         except:
-            raise IOError("Failed to read data from '%s'!" % datadir)
+            raise IOError("Failed to read file '%s'!" % salt)
 
-        # convert to .npz
-        npzfile = datadir + '/gdpy3-pickled-data-%s.npz' % salt[:10]
-        if not os.path.isfile(npzfile):
+        # extension
+        if 'extension' in kwargs:
+            ext = str(kwargs['extension'])
+            if ext not in ('npz', 'hdf5'):
+                log.error("Extension '%s' is not supported!" % ext)
+                ext = 'npz'
+        else:
+            ext = 'npz'
+        log.debug("Use extension '%s'." % ext)
+
+        # convert
+        savefile = 'gdpy3-pickled-data-%s.%s' % (salt[:10], ext)
+        savefile = os.path.join(datadir, savefile)
+        log.info("Pickled data file is %s." % savefile)
+        if not os.path.isfile(savefile):
             try:
-                gdc.convert(datadir, npzfile, **kwargs)
+                gdc.convert(datadir, savefile, **kwargs)
             except:
-                raise IOError("Failed to create file %s." % npzfile)
+                raise IOError("Failed to create file %s." % savefile)
 
+        # __init__
         self.datadir = datadir
-        self.file = npzfile
-        try:
-            tempf = numpy.load(self.file)
-            self.datakeys = tuple(tempf.files)
-            self.desc = str(tempf['description'])
-            self.description = self.desc
-        except (IOError, ValueError):
-            log.error("Failed to read file %s." % self.file)
-            raise
-        finally:
-            if 'tempf' in dir():
-                tempf.close()
+        if ext == 'npz':
+            ReadNpz.__init__(self, savefile)
+        elif ext == 'hdf5':
+            from .readhdf5 import ReadHdf5
+            ReadHdf5.__init__(self, savefile)
+        else:
+            log.error('Unknown extension.')
+
+    def __getitem__(self, key):
+        if os.path.splitext(self.file)[1] == '.npz':
+            return ReadNpz.__getitem__(self, key)
+        elif os.path.splitext(self.file)[1] == '.hdf5':
+            from .readhdf5 import ReadHdf5
+            return ReadHdf5.__getitem__(self, key)
+        else:
+            log.error('Unknown extension.')
