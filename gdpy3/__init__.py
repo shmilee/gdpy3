df24f90985199554f3e108c5301a6fa66911405e
shmilee <shmilee.zju@gmail.com>
2017-05-22 13:18:45 +0800

    bump version

--- gdpy3/__init__.py
+++ gdpy3/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "gdpy3"
 __doc__ = "Gyrokinetic Toroidal Code Data Processing tools written in python3"
 __author__ = "shmilee"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __status__ = "alpha"
 __license__ = "MIT"
 __email__ = "shmilee.zju@gmail.com"
