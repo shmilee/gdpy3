c4f1654bfb39e67b075f0b69b249c11db229a017
shmilee <shmilee.zju@gmail.com>
2017-06-06 20:14:11 +0800

    gdp: data1d, add residual_zonal_flow

--- gdpy3/plot/data1d.py
+++ gdpy3/plot/data1d.py
@@ -11,7 +11,6 @@ This module needs radial time data in group 'data1d' get by gdr.
 This module provides the :class:`Data1dFigureV110922`.
 '''
 
-import os
 import logging
 import numpy as np
 
@@ -29,9 +28,9 @@ class Data1dFigureV110922(GFigure):
     '''
     __slots__ = []
     ver = '110922'
-    __paragrp = 'gtcout/'
-    __keysgrp = 'data1d/'
-    __FigInfo = {
+    _paragrp = 'gtcout/'
+    _keysgrp = 'data1d/'
+    _FigInfo = {
         # data1di(0:mpsi,mpdata1d)
         'ion_flux': dict(
             key='i-particle-flux', title='thermal ion particle flux'),
@@ -56,6 +55,8 @@ class Data1dFigureV110922(GFigure):
         # field00(0:mpsi,nfield)
         'zonal_flow': dict(
             key='field00-phi', title='zonal flow'),
+        'residual_zonal_flow': dict(
+            key='field00-phi', title='residual zonal flow'),
         'zonal_current': dict(
             key='field00-apara', title='zonal current'),
         'zonal_fluidne': dict(
@@ -71,9 +72,9 @@ class Data1dFigureV110922(GFigure):
 
     def __init__(self, name, dataobj, figurestyle=['gdpy3-notebook']):
         grp = 'data1d'
-        if name not in self.__FigInfo.keys():
+        if name not in self._FigInfo.keys():
             raise ValueError("'%s' not found in group '%s'!" % (name, grp))
-        keys = [self.__keysgrp + self.__FigInfo[name]['key']]
+        keys = [self._keysgrp + self._FigInfo[name]['key']]
         super(Data1dFigureV110922, self).__init__(
             name, grp, dataobj, keys, figurestyle=figurestyle)
 
@@ -101,7 +102,7 @@ class Data1dFigureV110922(GFigure):
         self.calculation = {}
 
         Zkey = self.figurekeys[0]
-        tstep, ndiag = self.__paragrp + 'tstep', self.__paragrp + 'ndiag'
+        tstep, ndiag = self._paragrp + 'tstep', self._paragrp + 'ndiag'
         if tools.in_dictobj(self.gtcdataobj, Zkey, tstep, ndiag):
             Z = self.gtcdataobj[Zkey]
             if Z.size == 0:
@@ -135,16 +136,157 @@ class Data1dFigureV110922(GFigure):
 
         self.figurestructure['AxesStructures'] = [{
             'data': [
-                [1, plot_method, (X, Y, Z), dict(
-                    label='rtime', vmin=-Zmax, vmax=Zmax,
-                    **adddatadict)],
+                [1, plot_method, (X, Y, Z),
+                    dict(label='rtime', vmin=-Zmax, vmax=Zmax,
+                         **adddatadict)],
             ],
             'layout': [
                 111,
-                dict(title=self.__FigInfo[self.name]['title'],
+                dict(title=self._FigInfo[self.name]['title'],
                      xlabel=r'time($R_0/c_s$)', ylabel='radial',
                      **addlayoutdict)
             ],
             'revise': self.nginp.tool['get_colorbar_revise_func']('rtime'),
         }]
+
+        # residual zonal flow
+        if self.name == 'residual_zonal_flow':
+            # set axesstructures, calculation
+            return _set_reszf_axesstructures(self, X, Y, Z, Zmax, tunit)
+
         return True
+
+
+def _set_reszf_axesstructures(self, X, Y, Z, Zmax, tunit):
+    '''
+    Set residual zonal flow axesstructures, calculation
+    '''
+    dictobj = self.gtcdataobj
+    krrhoi, krrho0, istep, krdltr, qiflux, rgiflux = (
+        self._paragrp + key for key in
+        ['zfkrrhoi', 'zfkrrho0', 'zfistep', 'zfkrdltr', 'qiflux', 'rgiflux'])
+    if tools.in_dictobj(dictobj, krrhoi, krrho0, istep, krdltr):
+        krrhoi, krrho0, istep, krdltr, qiflux, rgiflux = dictobj.get_many(
+            krrhoi, krrho0, istep, krdltr, qiflux, rgiflux)
+    else:
+        return False
+    self.calculation.update({'krrhoi': krrhoi, 'krrho0': krrho0,
+                             'qiflux': qiflux, 'rgiflux': rgiflux})
+
+    # 1 original
+    log.debug("Getting Axes 221 ...")
+    axes1 = self.figurestructure['AxesStructures'][0]
+    axes1['layout'][0] = 221
+    axes1['layout'][1]['title'] = r'$\phi_{p00}, q=%.3f, \epsilon=%.3f$' % (
+        qiflux, rgiflux)
+
+    # 2 history, $\Delta r/2$,  $\Delta r/2 + \lambda/2$
+    log.debug("Getting Axes 222 ...")
+    index = tools.argrelextrema(Z.sum(axis=1))
+    if index.size < 3:
+        log.warn("Lines of peak less than 3!")
+        return False
+    i = int(len(index) / 2)
+    iZ1, iZ2 = index[i], index[i + 1]
+    Z1, Z2 = Z[iZ1, :], Z[iZ2, :]
+    # skip data before istep, maybe zeros
+    for i in range(0, Z1.size):
+        if Z1[i] != 0:
+            break
+    if i != istep:
+        log.warn("Find nozero in '%s', before istep: '%s'!" % (i, istep))
+    time = np.arange(istep, Z1.size) * tunit
+    Z1, Z2 = Z1[istep:] / abs(Z1[istep]), Z2[istep:] / abs(Z2[istep])
+    # find residual region
+    idx1, len1 = tools.findflat(Z1, 0.0005)
+    idx2, len2 = tools.findflat(Z2, 0.0005)
+    log.debug("Flat region: [%s,%s], [%s,%s]."
+              % (time[idx1], time[idx1 + len1 - 1],
+                 time[idx2], time[idx2 + len2 - 1]))
+    res1, res2 = sum(Z1[idx1:idx1 + len1]) / len1, \
+        sum(Z2[idx2:idx2 + len2]) / len2
+    axes2 = {
+        'layout': [222, dict(title=r'normalized $\phi_{p00}$, '
+                                   r'$k_r\rho_i=%.4f$,'
+                                   r'$k_r\rho_0=%.4f$' % (krrhoi, krrho0),
+                             xlim=[time[0], time[-1] + tunit],
+                             xlabel=r'time($R_0/c_s$)',)],
+        'data': [
+            [1, 'plot', (time, Z1), dict(
+                label=r'$r=%s, \phi_{res}=%.6f$' % (iZ1, abs(res1)))],
+            [2, 'plot', (time, Z2), dict(
+                label=r'$r=%s, \phi_{res}=%.6f$' % (iZ2, abs(res2)))],
+            [3, 'plot', (time[idx1:idx1 + len1],
+                         Z1[idx1:idx1 + len1], '--'), dict()],
+            [4, 'plot', (time[idx2:idx2 + len2],
+                         Z2[idx2:idx2 + len2], '--'), dict()],
+            [5, 'legend', (), dict()]
+        ],
+    }
+    self.figurestructure['AxesStructures'].append(axes2)
+    self.calculation.update({'ZFres1': abs(res1), 'ZFres2': abs(res2)})
+
+    # 3 gamma
+    log.debug("Getting Axes 223 ...")
+    logZ1 = np.log(abs(tools.savgol_golay_filter(Z1 - res1, 47, 3)))
+    logZ2 = np.log(abs(tools.savgol_golay_filter(Z2 - res2, 47, 3)))
+    xlim = [time[0], time[max(idx1, idx2)] + tunit]
+    idx1 = [i for i in tools.argrelextrema(logZ1, m='max') if i < idx1]
+    idx2 = [i for i in tools.argrelextrema(logZ2, m='max') if i < idx2]
+    tfit1, tfit2 = [time[i] for i in idx1], [time[i] for i in idx2]
+    lzfit1, lzfit2 = [logZ1[i] for i in idx1], [logZ2[i] for i in idx2]
+    if tfit1:
+        result, line1 = tools.fitline(tfit1, lzfit1, 1, info='%s max' % iZ1)
+        gamma1 = result[0][0]
+    else:
+        line1, gamma1 = [], 0
+    if tfit2:
+        result, line2 = tools.fitline(tfit2, lzfit2, 1, info='%s max' % iZ2)
+        gamma2 = result[0][0]
+    else:
+        line2, gamma2 = [], 0
+    axes3 = {
+        'layout': [223, dict(
+            xlim=xlim,
+            xlabel=r'time($R_0/c_s$)',
+            ylabel=r'log(abs(smooth($\phi_{p00} - \phi_{res}$)))')],
+        'data': [
+            [1, 'plot', (time, logZ1), dict(label=r'$r=%s$' % iZ1)],
+            [2, 'plot', (time, logZ2), dict(label=r'$r=%s$' % iZ2)],
+            [3, 'plot', (tfit1, line1, '--'),
+                dict(label=r'$\gamma_{%s}=%.6f$' % (iZ1, gamma1))],
+            [4, 'plot', (tfit2, line2, 'v--'),
+                dict(label=r'$\gamma_{%s}=%.6f$' % (iZ2, gamma2))],
+            [5, 'legend', (), dict()]
+        ],
+    }
+    self.figurestructure['AxesStructures'].append(axes3)
+    self.calculation.update({'GAMgamma1': gamma1, 'GAMgamma2': gamma2})
+
+    # 4 FFT, omega
+    log.debug("Getting Axes 224 ...")
+    f1, a1, p1 = tools.fft(tunit, Z1 - res1)
+    f2, a2, p2 = tools.fft(tunit, Z2 - res2)
+    index = int(time.size / 2)
+    omega1 = f1[index + np.argmax(p1[index:])]
+    omega2 = f2[index + np.argmax(p2[index:])]
+    xlim = 4 * max(omega1, omega2)
+    axes4 = {
+        'layout': [224, dict(
+            xlim=[-xlim, xlim],
+            xlabel=r'$\omega$($c_s/R_0$)',
+            ylabel=r'FFT($\phi_{p00} - \phi_{res}$)')],
+        'data': [
+                [1, 'plot', (f1, p1), dict(label=r'$r=%s$' % iZ1)],
+                [2, 'plot', (f2, p2), dict(label=r'$r=%s$' % iZ2)],
+                [3, 'axvline', (omega1,), dict(
+                    ls='--', label=r'$\omega_{%s}=%.6f$' % (iZ1, omega1))],
+                [4, 'axvline', (omega2,), dict(
+                    ls=':', label=r'$\omega_{%s}=%.6f$' % (iZ2, omega2))],
+                [5, 'legend', (), dict()]
+        ],
+    }
+    self.figurestructure['AxesStructures'].append(axes4)
+    self.calculation.update({'GAMomega1': omega1, 'GAMomega2': omega2})
+
+    return True
