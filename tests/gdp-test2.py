677f922bedfcf8dd0bac456c12db846cf9a8a350
shmilee <shmilee.zju@gmail.com>
2017-06-17 13:38:06 +0800

    test trackparticle orbits

--- tests/gdp-test2.py
+++ tests/gdp-test2.py
@@ -5,7 +5,7 @@
 
 import logging
 import gdpy3.read as gdr
-from gdpy3.plot import data1d, history
+from gdpy3.plot import data1d, history, trackparticle
 
 log0 = logging.getLogger('test')
 log1 = logging.getLogger('gdp')
@@ -17,31 +17,43 @@ CasePath = '/home/IFTS_shmilee/20170527-ITG-nonlinear/n14-D/'
 if __name__ == '__main__':
     log1.setLevel(10)
     dictobj = gdr.read(CasePath)
-    zf = data1d.Data1dFigureV110922(
+    # zf = data1d.Data1dFigureV110922(
+    #    dictobj,
+    #    'zonal_flow',
+    #    #'residual_zonal_flow',
+    #)
+    # his = history.HistoryFigureV110922(
+    #    dictobj,
+    #    #'ion_density_entropy',
+    #    #'ion_momentum',
+    #    #'ion_energy',
+    #    'ion_particle_momentum_flux',
+    #    #'ion_energy_flux',
+    #    #'field_phi',
+    #    #'field_phi00',
+    #    #'mode3_phi',
+    #)
+    track = trackparticle.TrackParticleFigureV110922(
         dictobj,
-        'zonal_flow',
-        #'residual_zonal_flow',
-    )
-    his = history.HistoryFigureV110922(
-        dictobj,
-        #'ion_density_entropy',
-        #'ion_momentum',
-        #'ion_energy',
-        'ion_particle_momentum_flux',
-        #'ion_energy_flux',
-        #'field_phi',
-        #'field_phi00',
-        #'mode3_phi',
+        'orbit_2d_ion',
+        #'orbit_3d_ion',
+        #'orbit_2d_electron',
     )
     for gf in [
             # zf,
-            his]:
+            # his,
+            track]:
         gf.calculate(
             # region_start=120, region_end=400,
             # region_start=20, region_end=120,
             # plot_method='plot_surface',
-            plot_method='pcolormesh',
+            # plot_method='pcolormesh',
+            # key='random', index=range(5,14),
+            key='in-', index=range(1, 10),
         )
+        if 'orbit_2d' in gf.name:
+            gf.figurestyle = ['gdpy3-notebook',
+                              {'figure.figsize': (12.0, 12.0)}]
         log0.info('calculation: %s ' % gf.calculation)
         input('Enter to continue, show: ')
         gf.show()
