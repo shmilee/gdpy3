2f06754f762a5d9ec8d24f1641394d9010e3f767
shmilee <shmilee.zju@gmail.com>
2017-06-20 19:52:31 +0800

    test gdp.snapshot

--- tests/gdp-test2.py
+++ tests/gdp-test2.py
@@ -5,7 +5,7 @@
 
 import logging
 import gdpy3.read as gdr
-from gdpy3.plot import data1d, history, trackparticle
+from gdpy3.plot import data1d, history, snapshot, trackparticle
 
 log0 = logging.getLogger('test')
 log1 = logging.getLogger('gdp')
@@ -17,32 +17,42 @@ CasePath = '/home/IFTS_shmilee/20170527-ITG-nonlinear/n14-D/'
 if __name__ == '__main__':
     log1.setLevel(10)
     dictobj = gdr.read(CasePath)
-    # zf = data1d.Data1dFigureV110922(
-    #    dictobj,
-    #    'zonal_flow',
-    #    #'residual_zonal_flow',
-    #)
-    # his = history.HistoryFigureV110922(
-    #    dictobj,
-    #    #'ion_density_entropy',
-    #    #'ion_momentum',
-    #    #'ion_energy',
-    #    'ion_particle_momentum_flux',
-    #    #'ion_energy_flux',
-    #    #'field_phi',
-    #    #'field_phi00',
-    #    #'mode3_phi',
-    #)
+    zf = data1d.Data1dFigureV110922(
+        dictobj,
+        'zonal_flow',
+        #'residual_zonal_flow',
+    )
+    his = history.HistoryFigureV110922(
+        dictobj,
+        #'ion_density_entropy',
+        #'ion_momentum',
+        #'ion_energy',
+        'ion_particle_momentum_flux',
+        #'ion_energy_flux',
+        #'field_phi',
+        #'field_phi00',
+        #'mode3_phi',
+    )
     track = trackparticle.TrackParticleFigureV110922(
         dictobj,
         'orbit_2d_ion',
         #'orbit_3d_ion',
         #'orbit_2d_electron',
     )
+    snap = snapshot.SnapshotFigureV110922(
+        dictobj,
+        #'ion_density',
+        'ion_flow',
+        #'ion_energy',
+        #'electron_flow',
+        #'fastion_density',
+        group='snap00200',
+    )
     for gf in [
             # zf,
             # his,
-            track]:
+            # track,
+            snap]:
         gf.calculate(
             # region_start=120, region_end=400,
             # region_start=20, region_end=120,
