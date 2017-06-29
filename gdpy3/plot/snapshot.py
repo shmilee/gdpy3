a50d1b822f3d5cddedecc814b64c8af0971d7fa8
shmilee <shmilee.zju@gmail.com>
2017-06-29 12:44:06 +0800

    gdp: add snapshot pdf figures; twinx profile, pdf

--- gdpy3/plot/snapshot.py
+++ gdpy3/plot/snapshot.py
@@ -15,7 +15,7 @@ import logging
 import numpy as np
 
 from . import tools
-from .gfigure import GFigure
+from .gfigure import GFigure, get_twinx_axesstructures
 
 __all__ = ['SnapshotFigureV110922']
 
@@ -29,20 +29,38 @@ class SnapshotFigureV110922(GFigure):
     __slots__ = []
     _FigGroup = 'snapshot'
     _ProfileFigInfo = {
-        '%s_%s' % (p, d[0]): dict(
-            index=d[1], title=d[2],
+        '%s_profile' % p: dict(
+            xlabel='r (mpsi)',
+            title='%s profile' % p,
+            twinx=[
+                dict(left=[(0, 'density f')],
+                     right=[(1, r'density $\delta f$')],
+                     lylabel='$f$', rylabel=r'$\delta f$'),
+                dict(left=[(2, 'flow f')], right=[(3, r'flow $\delta f$')],
+                     lylabel='$f$', rylabel=r'$\delta f$'),
+                dict(left=[(4, 'energy f')],
+                     right=[(5, r'energy $\delta f$')],
+                     lylabel='$f$', rylabel=r'$\delta f$'),
+            ],
             srckey=['mpsi+1', '%s-profile' % p])
         for p in ['ion', 'electron', 'fastion']
-        for d in [
-            ['density', [0, 1],
-                [r'%s density full f' % p, r'%s density $\delta f$' % p]],
-            ['flow', [2, 3],
-                [r'%s flow full f' % p, r'%s flow $\delta f$' % p]],
-            ['energy', [4, 5],
-                [r'%s energy full f' % p, r'%s energy $\delta f$' % p]],
-        ]
     }
-    _FigInfo = dict(_ProfileFigInfo)
+    _PdfFigInfo = {
+        '%s_pdf' % p: dict(
+            xlabel='nvgrid',
+            title='%s pdf' % p,
+            twinx=[
+                dict(left=[(0, 'energy f')],
+                     right=[(1, r'energy $\delta f$')],
+                     lylabel='$f$', rylabel=r'$\delta f$'),
+                dict(left=[(2, 'pitch angle f')],
+                     right=[(3, r'pitch angle $\delta f$')],
+                     lylabel='$f$', rylabel=r'$\delta f$'),
+            ],
+            srckey=['nvgrid', '%s-pdf' % p])
+        for p in ['ion', 'electron', 'fastion']
+    }
+    _FigInfo = dict(_ProfileFigInfo, **_PdfFigInfo)
 
     def __init__(self, dataobj, name,
                  group=None, figurestyle=['gdpy3-notebook']):
@@ -60,6 +78,10 @@ class SnapshotFigureV110922(GFigure):
 
         Notes
         -----
+        1. profile, pdf kwargs:
+           hspace: float, subplot.hspace, default 0.02
+           xlim: (`left`, `right`), default [0, max(X)]
+           ylabel_rotation: str or int, default 'vertical'
         '''
         log.debug("Get FigureStructure, calculation of '%s' ..." % self.Name)
         self.figurestructure = {
@@ -68,48 +90,54 @@ class SnapshotFigureV110922(GFigure):
         }
         self.calculation = {}
 
-        if self.name in self._ProfileFigInfo:
-            return _set_profile_axesstructures(self, **kwargs)
+        if self.name in self._ProfileFigInfo or self.name in self._PdfFigInfo:
+            if 'hspace' in kwargs and isinstance(kwargs['hspace'], float):
+                hspace = kwargs['hspace']
+            else:
+                hspace = 0.02
+            self.figurestructure['Style'] = self.figurestyle + \
+                [{'figure.subplot.hspace': hspace}]
+            return _set_profile_or_pdf_axesstructures(self, **kwargs)
         else:
             return False
 
 
-def _set_profile_axesstructures(self, **kwargs):
+def _set_profile_or_pdf_axesstructures(self, **kwargs):
     '''
-    Set profile axesstructures, calculation
+    Set particle profile, pdf axesstructures, calculation
     '''
 
     # check key, get data
-    index = self.figureinfo['index']
+    xlabel = self.figureinfo['xlabel']
     title = self.figureinfo['title']
-    if len(index) != len(title):
-        log.error("Invalid figure info!")
-        return False
-    else:
-        length = len(index)
-    mpsi1, profile = self.figureinfo['key']
+    twinx = self.figureinfo['twinx']
+    # when x='mpsi+1', ydata='profile'
+    # when x='nvgrid', ydata='pdf'
+    x, ydata = self.figureinfo['key']
     try:
-        mpsi1, profile = self.dataobj.get_many(mpsi1, profile)
-        ypro = [profile[:, index[i]] for i in range(length)]
+        time = ' (istep=%d)' % int(self.group.replace('snap', ''))
+        title = title + time
+        x, ydata = self.dataobj.get_many(x, ydata)
+        if ydata.size == 0:
+            log.debug("No data for Figure '%s'." % self.Name)
+            return False
+        X = np.arange(x)
+        Ydata = ydata.T
     except Exception as exc:
         log.error("Failed to get data of '%s' from %s! %s" %
                   (self.Name, self.dataobj.file, exc))
         return False
 
-    for i in range(length):
-        number = int("%s1%s" % (length, i + 1))
-        log.debug("Getting Axes %s ..." % number)
-        axes = {
-            'data': [
-                [1, 'plot', (range(mpsi1), ypro[i]), dict(label=title[i])],
-                [2, 'legend', (), dict()],
-            ],
-            'layout': [
-                number, dict(xlabel='radial (mpsi)',
-                             xlim=[0, mpsi1],
-                             **{'title': title[i] if i == 0 else ''})
-            ],
-        }
-        self.figurestructure['AxesStructures'].append(axes)
+    if 'xlim' not in kwargs:
+        kwargs['xlim'] = [0, np.max(X)]
+
+    try:
+        axesstructures = get_twinx_axesstructures(
+            X, Ydata, xlabel, title, twinx, **kwargs)
+        self.figurestructure['AxesStructures'] = axesstructures
+    except Exception as exc:
+        log.error("Failed to set AxesStructures of '%s'! %s"
+                  % (self.Name, exc))
+        return False
 
     return True
