9405885ef86885de72b912f066d0210a32ffe730
shmilee <shmilee.zju@gmail.com>
2017-07-03 16:43:44 +0800

    gdp: snapshot, add fieldspectrum, fieldploidal figures

--- gdpy3/plot/snapshot.py
+++ gdpy3/plot/snapshot.py
@@ -71,7 +71,23 @@ class SnapshotFigureV110922(GFigure):
             srckey=['fluxdata-%s' % f])
         for f in ['phi', 'apara', 'fluidne']
     }
-    _FigInfo = dict(_ProfileFigInfo, **_PdfFigInfo, **_FieldFluxFigInfo)
+    _FieldSpectrumFigInfo = {
+        '%s_spectrum' % f: dict(
+            field=r'$%s$' % f.replace(
+                'phi', '\phi').replace('apara', 'a_{\parallel}'),
+            srckey=['mtgrid+1', 'mtoroidal', 'fluxdata-%s' % f])
+        for f in ['phi', 'apara', 'fluidne']
+    }
+    _FieldPloidalFigInfo = {
+        '%s_ploidal' % f: dict(
+            title=r'$%s$ on ploidal plane' % f.replace(
+                'phi', '\phi').replace('apara', 'a_{\parallel}'),
+            srckey=['mpsi+1', 'mtgrid+1',
+                    'poloidata-x', 'poloidata-z', 'poloidata-%s' % f])
+        for f in ['phi', 'apara', 'fluidne']
+    }
+    _FigInfo = dict(_ProfileFigInfo, **_PdfFigInfo, **_FieldFluxFigInfo,
+                    **_FieldSpectrumFigInfo, **_FieldPloidalFigInfo)
 
     def __init__(self, dataobj, name,
                  group=None, figurestyle=['gdpy3-notebook']):
@@ -93,11 +109,13 @@ class SnapshotFigureV110922(GFigure):
            *hspace*: float, subplot.hspace, default 0.02
            *xlim*: (`left`, `right`), default [0, max(X)]
            *ylabel_rotation*: str or int, default 'vertical'
-        2. fieldflux kwargs:
+        2. fieldflux, fieldploidal kwargs:
            *plot_method*, *plot_args*, *plot_kwargs*,
            *colorbar*, *grid_alpha*, *surface_contourf*
            keyword arguments are passed on to
            :func:`gdpy3.plot.gfigure.get_pcolor_axesstructures`
+        3. fieldspectrum kwargs:
+           *mmode*, *pmode*
         '''
         log.debug("Get FigureStructure, calculation of '%s' ..." % self.Name)
         self.figurestructure = {
@@ -116,6 +134,10 @@ class SnapshotFigureV110922(GFigure):
             return _set_profile_or_pdf_axesstructures(self, **kwargs)
         elif self.name in self._FieldFluxFigInfo:
             return _set_fieldflux_axesstructures(self, **kwargs)
+        elif self.name in self._FieldSpectrumFigInfo:
+            return _set_fieldspectrum_axesstructures(self, **kwargs)
+        elif self.name in self._FieldPloidalFigInfo:
+            return _set_fieldploidal_axesstructures(self, **kwargs)
         else:
             return False
 
@@ -194,6 +216,123 @@ def _set_fieldflux_axesstructures(self, **kwargs):
     try:
         axesstructures = get_pcolor_axesstructures(
             X, Y, fluxdata, r'$\zeta$', r'$\theta$', title, **kwargs)
+        self.figurestructure['AxesStructures'] = axesstructures
+    except Exception as exc:
+        log.error("Failed to set AxesStructures of '%s'! %s"
+                  % (self.Name, exc))
+        return False
+
+    return True
+
+
+def _set_fieldspectrum_axesstructures(self, **kwargs):
+    '''
+    Set field poloidal and parallel spectra axesstructures, calculation
+    '''
+
+    # check key
+    mtgrid1, mtoroidal, fluxdata = self.figureinfo['key']
+    field = self.figureinfo['field']
+    try:
+        mtgrid1, mtoroidal, fluxdata = self.dataobj.get_many(
+            mtgrid1, mtoroidal, fluxdata)
+        if fluxdata.size == 0:
+            log.debug("No data for Figure '%s'." % self.Name)
+            return False
+        if fluxdata.shape != (mtgrid1, mtoroidal):
+            log.error("Invalid fluxdata shape!")
+            return False
+        mtgrid = mtgrid1 - 1
+        maxmmode = int(mtgrid / 2 + 1)
+        maxpmode = int(mtoroidal / 2 + 1)
+        mmode = mtgrid // 5
+        pmode = mtoroidal // 3
+        if ('mmode' in kwargs and isinstance(kwargs['mmode'], (int, float))
+                and int(kwargs['mmode']) <= maxmmode):
+            mmode = int(kwargs['mmode'])
+        if ('pmode' in kwargs and isinstance(kwargs['pmode'], (int, float))
+                and int(kwargs['pmode']) <= maxpmode):
+            pmode = int(kwargs['pmode'])
+        log.info("Poloidal and parallel range: m=%s, p=%s. Maximal m=%s, p=%s"
+                 % (mmode, pmode, maxmmode, maxpmode))
+        X1, Y1 = np.arange(1, mmode + 1), np.zeros(mmode)
+        X2, Y2 = np.arange(1, pmode + 1), np.zeros(pmode)
+        for i in range(mtoroidal):
+            yy = np.fft.fft(fluxdata[:, i])
+            Y1[0] = Y1[0] + (abs(yy[0]))**2
+            for j in range(1, mmode):
+                Y1[j] = Y1[j] + (abs(yy[j]))**2 + (abs(yy[mtgrid - j]))**2
+        Y1 = np.sqrt(Y1 / mtoroidal) / mtgrid
+        for i in range(mtgrid):
+            yy = np.fft.fft(fluxdata[i, :])
+            Y2[0] = Y2[0] + (abs(yy[0]))**2
+            for j in range(1, pmode):
+                Y2[j] = Y2[j] + (abs(yy[j]))**2 + (abs(yy[mtoroidal - j]))**2
+        Y2 = np.sqrt(Y2 / mtgrid) / mtoroidal
+    except Exception as exc:
+        log.error("Failed to get data of '%s' from %s! %s" %
+                  (self.Name, self.dataobj.file, exc))
+        return False
+
+    for ax in [[211, (X1, Y1, 'o-'), 'poloidal', [0, mmode], 'mtgrid'],
+               [212, (X2, Y2, 'o-'), 'parallel', [1, pmode], 'mtoroidal']]:
+        log.debug("Getting Axes %s ..." % ax[0])
+        axes = {
+            'data': [
+                    [1, 'plot', ax[1],
+                        dict(label='m=%d, p=%d' % (mmode, pmode))],
+                    [2, 'legend', (), dict(loc='best')],
+            ],
+            'layout': [
+                ax[0],
+                dict(title='%s %s spectrum' % (field, ax[2]),
+                     xlim=ax[3], xlabel=ax[4])
+            ],
+        }
+        self.figurestructure['AxesStructures'].append(axes)
+
+    return True
+
+
+def _set_fieldploidal_axesstructures(self, **kwargs):
+    '''
+    Set phi, apara, fluidne on ploidal plane axesstructures, calculation
+    '''
+
+    mpsi1, mtgrid1, xdata, zdata, pdata = self.figureinfo['key']
+    title = self.figureinfo['title']
+    try:
+        mpsi1, mtgrid1, xdata, zdata, pdata = self.dataobj.get_many(
+            mpsi1, mtgrid1, xdata, zdata, pdata)
+        if pdata.size == 0:
+            log.debug("No data for Figure '%s'." % self.Name)
+            return False
+    except Exception as exc:
+        log.error("Failed to get data of '%s' from %s! %s" %
+                  (self.Name, self.dataobj.file, exc))
+        return False
+
+    # default contourf,  levels 200
+    if 'plot_method' not in kwargs:
+        kwargs['plot_method'] = 'contourf'
+        if 'plot_args' not in kwargs:
+            kwargs['plot_args'] = [200]
+
+    # fix 3d plot_surface cmap
+    if 'plot_method' in kwargs and kwargs['plot_method'] == 'plot_surface':
+        cmap = self.nginp.tool['get_style_param'](
+            self.figurestyle, 'image.cmap')
+        if ('plot_kwargs' in kwargs
+                and isinstance(kwargs['plot_kwargs'], dict)):
+            kwargs['plot_kwargs']['cmap'] = cmap
+        else:
+            kwargs['plot_kwargs'] = dict(cmap=cmap)
+
+    try:
+        axesstructures = get_pcolor_axesstructures(
+            xdata, zdata, pdata, r'$X(R_0)$', r'$Z(R_0)$', title, **kwargs)
+        data = axesstructures[0]['data']
+        data.append([len(data) + 1, 'set_aspect', ('equal',), dict()])
         self.figurestructure['AxesStructures'] = axesstructures
     except Exception as exc:
         log.error("Failed to set AxesStructures of '%s'! %s"
