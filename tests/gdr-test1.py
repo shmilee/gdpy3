90975d91355b42f88ca8da218e9da54319fcfb65
shmilee <shmilee.zju@gmail.com>
2017-08-29 20:45:57 +0800

    update gdr-test

--- tests/gdr-test1.py
+++ tests/gdr-test1.py
@@ -2,32 +2,34 @@
 
 # Copyright (c) 2017 shmilee
 
-import os
-import logging
+import sys
 import gdpy3.read as gdr
+import gdpy3.glogger
 
-log = logging.getLogger('gdr')
+log = gdpy3.glogger.getGLogger('gdr')
+
 
 if __name__ == '__main__':
-    # log.setLevel(20)
-    log.setLevel(10)
-    datadir = '/home/IFTS_shmilee/phiobo-4-test'
-    numpat = r'[-+]?\d+[\.]?\d*[eE]?[-+]?\d*'
-    mypats = [r'\*{6}\s+k_r\*rhoi=\s*?(?P<krrhoi>' + numpat + r'?)\s*?,'
-              + r'\s+k_r\*rho0=\s*?(?P<krrho0>' + numpat + r'?)\s+?\*{6}',
-              r'\*{5}\s+k_r\*dlt_r=\s*?(?P<krdltr>' + numpat + r'?)\s+?\*{5}']
-    obj = gdr.read(datadir,
-                   description='set kr*rho=0.1, q=1.4, e=0.2',
-                   version='110922',
-                   extension='hdf5',
-                   # extension='npz',
-                   additionalpats=mypats)
-    print('1. obj.datadir:\n%s\n' % obj.datadir)
-    print('2. obj.file:\n%s\n' % obj.file)
-    print('3. obj.datakeys:\n%s\n' % str(obj.datakeys))
-    print('4. obj.desc:\n%s\n' % obj.desc)
-    print('5. obj[obj.datakeys[10]]:\n%s\n' % obj[obj.datakeys[10]])
-    print("   obj['gtcout/krdltr']:  %s\n" % obj['gtcout/krdltr'])
-    print("   obj['gtcout/krrhoi']:  %s\n" % obj['gtcout/krrhoi'])
-    print("6. obj.keys():\n%s\n" % str(obj.keys()))
-    print("7. obj.find('phi', 'field'):\n%s\n" % str(obj.find('phi', 'field')))
+    log.setLevel(20)
+    #log.handlers[0].setLevel(10)
+    print(log, log.handlers)
+    dataobj = gdr.read(path=sys.argv[1],
+                       description='... test desc test ...',
+                       version='110922',
+                       extension='hdf5',
+                       #extension='npz',
+                       overwrite=True,
+                       )
+    print('1. desc:\n%s\n' % dataobj.desc)
+    print('2. file:\n%s\n' % dataobj.file)
+    print('3. datakeys[10]]:\n%s\n' % dataobj[dataobj.datakeys[10]])
+    print("   dataobj['gtc/r0']:  %s\n" % dataobj['gtc/r0'])
+    print("4. dataobj.find('phi', 'field'):\n%s\n" %
+          str(dataobj.find('phi', 'field')))
+
+    keys = ['gtc/b0', 'gtc/rho0']
+    print('5. first: %s\n%s\n' % (keys, dataobj.get_many(*keys)))
+    print('6. second: %s\n%s\n' % (keys, dataobj.get_many(*keys)))
+    keys.extend(['data1d/field00-phi', 'gtc/start_date'])
+    print('7. first: %s\n%s\n' % (keys, dataobj.get_many(*keys)))
+    print('8. cache:\n%s' % dataobj.cache)
