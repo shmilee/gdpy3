20fa85de2e6aae6fc7760d536e00a818caff798f
shmilee <shmilee.zju@gmail.com>
2017-09-10 18:13:22 +0800

    use HTMLTestRunner to generate Test Report

---
+++ tests-run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 shmilee
+
+import os
+import unittest
+
+from HTMLTestRunner import HTMLTestRunner
+
+if __name__ == "__main__":
+    start_dir = os.path.join(os.path.dirname(__file__), 'gdpy3')
+    start_dir = os.path.dirname(__file__)
+    print("Loader discover start_dir: '%s'" % start_dir)
+    loader = unittest.TestLoader()
+    cases = loader.discover(start_dir=start_dir)
+    tests = unittest.TestSuite()
+    tests.addTests(cases)
+
+    with open("tests-report.html", 'wb') as fo:
+        runner = HTMLTestRunner(stream=fo,
+                                verbosity=2,
+                                title='Gdpy3 Test Report',
+                                description='')
+        runner.run(tests)
