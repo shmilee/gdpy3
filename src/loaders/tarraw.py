5084957b070e19334d7256a727ff995989f0e14e
shmilee <shmilee.zju@gmail.com>
2018-01-05 10:42:24 +0800

    add tar raw loader

---
+++ src/loaders/tarraw.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 shmilee
+
+'''
+Contains TarFile raw loader class.
+'''
+
+import os
+import tarfile
+
+from ..glogger import getGLogger
+from .base import BaseRawLoader
+
+__all__ = ['TarRawLoader']
+log = getGLogger('L')
+
+class TarRawLoader(BaseRawLoader):
+    # https://docs.python.org/3/library/tarfile.html
+    '''
+    Load raw data from a tar archive. Return a dictionary-like object.
+    '''
+    __slots__ = []
+
+    def _special_check_path(self):
+        if os.path.isfile(self.path) and tarfile.is_tarfile(self.path):
+            return True
+        else:
+            log.error("'%s' is not a tar archive file!" % self.path)
+            return False
+
+    def _special_open(self):
+        return tarfile.open(self.path, mode='r')
+
+    def _special_close(self, tmpobj):
+        tmpobj.close()
+
+    def _special_getkeys(self, tmpobj):
+        return sorted(
+            [n for n in tmpobj.getnames() if tmpobj.getmember(n).isfile()])
+
+    def _special_getfile(self, tmpobj, key):
+        return tmpobj.extractfile(key)
