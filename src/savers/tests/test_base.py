235b16160daaa70a3c71c368115cf56787475aa6
shmilee <shmilee.zju@gmail.com>
2018-02-09 22:34:45 +0800

    add unittest for BasePckSaver

--- src/savers/tests/test_base.py
+++ src/savers/tests/test_base.py
@@ -6,42 +6,48 @@ import os
 import unittest
 import tempfile
 
-from ..base import BaseFileSaver
+from ..base import BasePckSaver
 
 
-class TestBaseFileSaver(unittest.TestCase):
+class TestBasePckSaver(unittest.TestCase):
     '''
-    Test class BaseFileSaver
+    Test class BasePckSaver
     '''
 
     def setUp(self):
         self.tmp = tempfile.mktemp(suffix='-test')
-        self.tmpfile = self.tmp + BaseFileSaver._extension
+        self.tmpfile = self.tmp + BasePckSaver._extension
 
     def tearDown(self):
         if os.path.isfile(self.tmpfile):
             os.remove(self.tmpfile)
 
-    def test_filesaver_init(self):
-        saver = BaseFileSaver(self.tmp)
-        self.assertTrue(saver.file == self.tmpfile)
-        self.assertIsNone(saver.fobj)
+    def test_pcksaver_init(self):
+        saver = BasePckSaver(self.tmp)
+        self.assertTrue(saver.path == self.tmpfile)
+        self.assertIsNone(saver._storeobj)
         with self.assertRaises(IOError):
-            BaseFileSaver(os.path.join(self.tmpfile, 'BreakSuffix'))
+            BasePckSaver(os.path.join(self.tmpfile, 'BreakSuffix'))
 
-    def test_filesaver_iopen(self):
-        saver = BaseFileSaver(self.tmpfile)
+    def test_pcksaver_iopen(self):
+        saver = BasePckSaver(self.tmpfile)
         with self.assertRaises(NotImplementedError):
             saver.iopen()
 
-    def test_filesaver_write(self):
-        saver = BaseFileSaver(self.tmpfile)
-        with open(saver.file, mode='w') as saver.fobj:
+    def test_pcksaver_write(self):
+        saver = BasePckSaver(self.tmpfile)
+        with open(saver.path, mode='w') as saver._storeobj:
+            saver.status = True
             with self.assertRaises(NotImplementedError):
                 saver.write('g', {'t': 1})
 
-    def test_filesaver_close(self):
-        saver = BaseFileSaver(self.tmpfile)
-        with open(saver.file, mode='w') as saver.fobj:
-            with self.assertRaises(NotImplementedError):
-                saver.close()
+    def test_pcksaver_close(self):
+        saver = BasePckSaver(self.tmpfile)
+        with open(saver.path, mode='w') as saver._storeobj:
+            saver.status = True
+            saver.close()
+            self.assertIsNone(saver._storeobj)
+
+    def test_pcksaver_get_store(self):
+        saver = BasePckSaver(self.tmpfile)
+        self.assertEqual(self.tmpfile, saver.get_store())
