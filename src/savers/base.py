ecbdeeb56427700f9fa96ea42b25595f19e260d4
shmilee <shmilee.zju@gmail.com>
2018-02-09 22:22:21 +0800

    rewrite BaseFileSaver -> BasePckSaver

--- src/savers/base.py
+++ src/savers/base.py
@@ -10,118 +10,127 @@ import os
 
 from ..glogger import getGLogger
 
-__all__ = ['BaseFileSaver']
+__all__ = ['BasePckSaver']
 log = getGLogger('S')
 
 
-class BaseFileSaver(object):
+class BasePckSaver(object):
     '''
-    Save dict data with a group name to a file.
+    Save arrays data in dict with a group name to a file or cache.
 
     Attributes
     ----------
-    file: path of the file
-    fobj: the initialized file object
+    path: str
+        path of the file or cache name
+    status: bool
+        True, store object open; False, store object closed
 
     Parameters
     ----------
-    file : str, path of the file
+    path: str
 
-    Methods
-    -------
-    iopen()
-    write(group, data)
-    close()
+    Notes
+    -----
+    1. Instances of the class can be used with the ``with`` statement.
+    2. Use :meth:`iopen` to open saver, then :meth:`write` data,
+       finally, remember to :meth:`close` saver.
+    3. :meth:`get_store` is for cooperation with
+       :class:`gdpy3.loaders.base.BasePckLoader`.
     '''
-    __slots__ = ['file', 'fobj']
-    _extension = '.extension-of-file'
+    __slots__ = ['path', '_storeobj', 'status']
+    _extension = '.extension-of-path'
 
-    def __init__(self, file):
-        file = self._setpathname(file, self._extension)
-        filedir = os.path.dirname(file)
-        if os.access(filedir, os.W_OK):
-            self.file = file
-            self.fobj = None
-        else:
-            raise IOError("Can't access directory '%s'!" % filedir)
+    def _check_path_access(self):
+        '''Check for access to *path*.'''
+        pathdir = os.path.dirname(self.path) or '.'
+        return (os.access(self.path, os.W_OK) or os.access(pathdir, os.W_OK))
 
-    @staticmethod
-    def _setpathname(pathname, ext):
-        '''
-        If pathname extension is not ``ext``, change it's extension
-        '''
-        if os.path.splitext(pathname)[1] != ext:
-            return pathname + ext
-        else:
-            return pathname
+    def _check_path_exists(self):
+        '''*path* exists or not'''
+        return os.path.exists(self.path)
 
     def _open_append(self):
         '''
-        return `append` mode file object
+        Return `append` mode store object.
         '''
         raise NotImplementedError()
 
     def _open_new(self):
         '''
-        return `write` mode file object
+        Return `write` mode store object.
         '''
         raise NotImplementedError()
 
     def _write(self, group, data):
         '''
-        Write *data* to file object.
+        Write *data* to store object.
         '''
         raise NotImplementedError()
 
     def _close(self):
         '''
-        Close file object.
+        Close store object.
         '''
-        raise NotImplementedError()
+        self._storeobj.close()
+        self._storeobj = None
+
+    def get_store(self):
+        '''Return store path or object.'''
+        return self.path
+
+    def __init__(self, path):
+        self.path = path
+        self._storeobj = None
+        self.status = False
+        if not self._check_path_access():
+            raise IOError("Can't access path '%s'!" % self.path)
+        _p, ext = os.path.splitext(self.path)
+        if ext != self._extension:
+            log.warn("Path's extension should be '%s', not '%s'!"
+                     % (self._extension, ext))
+            self.path = _p + self._extension
 
     def iopen(self):
         '''
-        Initialize file object.
-        Open ``file`` if exists, create otherwise.
+        Initialize store object.
+        Open *path* if exists, create otherwise.
         '''
-
-        if self.fobj is not None:
-            log.warn("The file object has been initialized.")
+        if self.status:
+            log.warn("The store object has been initialized.")
             return
-        file = self.file
-        if os.path.isfile(file):
+        path = self.path
+        if self._check_path_exists():
             try:
-                log.debug("Open file '%s' to append data." % file)
-                self.fobj = self._open_append()
-                return
+                log.debug("Open path '%s' to append data." % path)
+                self._storeobj = self._open_append()
+                self.status = True
             except Exception:
-                log.error("Failed to open file '%s'." % file, exc_info=1)
+                log.error("Failed to open path '%s'." % path, exc_info=1)
                 raise
         else:
             try:
-                log.debug("Create file '%s' to store data." % file)
-                self.fobj = self._open_new()
-                return
+                log.debug("Create path '%s' to store data." % path)
+                self._storeobj = self._open_new()
+                self.status = True
             except Exception:
-                log.error("Failed to create file '%s'." % file, exc_info=1)
+                log.error("Failed to create path '%s'." % path, exc_info=1)
                 raise
 
     def write(self, group, data):
         '''
-        Write dict *data* with *group* name to file object.
+        Write dict *data* with *group* name to store object.
 
         Parameters
         ----------
         group: str, group name
-        data: dict data in this group
+        data: dict, data in this *group*
         '''
-
-        if self.fobj is None:
-            log.error("File object is not initialized!")
+        if not self.status:
+            log.error("Store object is not initialized!")
             return False
         else:
             if not (isinstance(group, str) and isinstance(data, dict)):
-                log.error("``group`` is not str, or ``data`` is not dict!")
+                log.error("'group' is not str, or 'data' is not dict!")
                 return False
             else:
                 self._write(group, data)
@@ -131,7 +140,14 @@ class BaseFileSaver(object):
         '''
         Close initialized file object.
         '''
-        if self.fobj is not None:
-            log.debug("Close initialized file '%s'." % self.file)
+        if self.status:
+            log.debug("Close path '%s'." % self.path)
             self._close()
-            self.fobj = None
+            self.status = False
+
+    def __enter__(self):
+        self.iopen()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
