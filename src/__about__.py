b47daa7076fed04ddb5662856e246d4cbeaa62ca
shmilee <shmilee.zju@gmail.com>
2018-07-05 14:26:30 +0800

    pre __data_path__  for non-py files

--- src/__about__.py
+++ src/__about__.py
@@ -12,3 +12,32 @@ __author__ = "shmilee"
 __email__ = "shmilee.zju@gmail.com"
 __license__ = "MIT"
 __copyright__ = 'Copyright (c) 2018 shmilee'
+
+
+def _get_data_path(name):
+    '''Get the path to *name*.'''
+    import os
+    import sys
+
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), name)
+    if os.path.isdir(path):
+        return path
+
+    # pyinstaller frozen check
+    if getattr(sys, 'frozen', None):
+        path = os.path.join(sys._MEIPASS, name)
+        if os.path.isdir(path):
+            return path
+
+        path = os.path.join(os.path.dirname(sys.executable), name)
+        if os.path.isdir(path):
+            return path
+
+        path = os.path.join(sys.path[0], name)
+        if os.path.isdir(path):
+            return path
+
+    raise RuntimeError("Can't find the %s files!" % name)
+
+
+__data_path__ = _get_data_path('gdpy3-data')
