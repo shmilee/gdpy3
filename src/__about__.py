aafc8fdd8967c3997da79c551b49e32bbddd4ac3
shmilee <shmilee.zju@gmail.com>
2020-12-05 23:02:44 +0800

    bump version

--- src/__about__.py
+++ src/__about__.py
@@ -5,7 +5,7 @@
 import os
 import sys
 
-VERSION = (0, 7, 1)
+VERSION = (0, 7, 2)
 
 __description__ = "Gyrokinetic Toroidal Code Data Processing tools written in python3"
 __url__ = "https://github.com/shmilee/gdpy3.git"
