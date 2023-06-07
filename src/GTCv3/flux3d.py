0755bfd7f77db850753f1a20d67c7693e9423711
shmilee <shmilee.zju@gmail.com>
2023-06-07 18:13:18 +0800

    convert & plot Flux3D coordinates

--- src/GTCv3/flux3d.py
+++ src/GTCv3/flux3d.py
@@ -14,6 +14,13 @@ shmilee.F90, shmilee_flux3d_diag, subroutine flux3d_loop
         write(ioflux3d,101) mtheta(i)
     enddo
     write(ioflux3d,102) flux3d
+
+shmilee.F90, shmilee_flux3d_diag, subroutine flux3d_once
+    write(ioflux3d,101) flux3d_iflux-diflux, flux3d_iflux+diflux, digrid, mtoroidal
+    do i=flux3d_iflux-diflux,flux3d_iflux+diflux
+        write(ioflux3d,101) mtheta(i)
+    enddo
+    write(ioflux3d,102) coord3d
 '''
 
 import re
@@ -28,9 +35,9 @@ from .snapshot import (SnapshotFieldFluxDigger,
 from .gtc import Ndigits_tstep
 from .. import tools
 
-_all_Converters = ['Flux3DConverter']
+_all_Converters = ['Flux3DConverter', 'Flux3DCoordConverter']
 _all_Diggers = ['Flux3DDigger', 'Flux3DTileDigger',
-                'Flux3DCorrLenDigger']
+                'Flux3DCorrLenDigger', 'Flux3DCoordDigger']
 __all__ = _all_Converters + _all_Diggers
 
 
@@ -46,7 +53,6 @@ class Flux3DConverter(Converter):
     nitems = '?'
     itemspattern = ['^phi_dir/(?P<section>flux3d\d{5,7})\.out$',
                     '.*/phi_dir/(?P<section>flux3d\d{5,7})\.out$']
-    # common for xyz, data same info, like nfield etc.
     _datakeys = (
         # 1. parameters
         'iflux0', 'iflux1', 'digrid', 'mtoroidal', 'nfield',
@@ -101,6 +107,58 @@ class Flux3DConverter(Converter):
                 clog.debug("Filling datakeys: %s ..." % key)
                 sd[key] = tmpdata[igrid0:igrid1, :, nf]
         # _ = sd.pop('mtoroidal')
+        return sd
+
+
+class Flux3DCoordConverter(Converter):
+    '''
+    cartesian coordinates X(theta, zeta, psi), Y, Z for field flux3d
+    Shape of data is (mtheta(ipsi), mtoroidal) for each ipsi
+    '''
+    __slot__ = []
+    nitems = '?'
+    itemspattern = ['^phi_dir/(?P<section>flux3dcoord)\.out$',
+                    '.*/phi_dir/(?P<section>flux3dcoord)\.out$']
+    _datakeys = (
+        # 1. parameters
+        'iflux0', 'iflux1', 'digrid', 'mtoroidal', r'mtgrid+1-%d',  # ipsi
+        # 2. allcoord(mtgrid+1, mtoroidal), iflux0<=ipsi<=iflux1
+        'X', 'Y', 'Z',  # -%d % ipsi
+    )
+
+    def _convert(self):
+        '''Read 'phi_dir/flux3dcoord.out' % istep.'''
+        with self.rawloader.get(self.files) as f:
+            clog.debug("Read file '%s'." % self.files)
+            outdata = f.readlines()
+
+        sd = {}
+        # 1. parameters
+        clog.debug("Filling datakeys: %s ..." % str(self._datakeys[:4]))
+        for i, key in enumerate(self._datakeys[:4]):
+            sd.update({key: int(outdata[i].strip())})
+        mtgrid_dict = {
+            ipsi: int(outdata[4+i].strip())
+            for i, ipsi in enumerate(range(sd['iflux0'], sd['iflux1']+1))}
+        idx0 = 4 + len(mtgrid_dict)
+        # 2. X Y Z-ipsi
+        tmpdata = np.array([float(n.strip()) for n in outdata[idx0:]])
+        shape = (sd['digrid'], sd['mtoroidal'], 3)
+        size = sd['digrid'] * sd['mtoroidal'] * 3
+        assert len(tmpdata) == size
+        tmpdata = tmpdata.reshape(shape, order='F')
+        igrid1 = 0
+        for ipsi in range(sd['iflux0'], sd['iflux1']+1):
+            mtgrid1 = mtgrid_dict[ipsi] + 1
+            key = r'mtgrid+1-%d' % ipsi
+            clog.debug("Filling datakeys: %s=%d ..." % (key, mtgrid1))
+            sd[key] = mtgrid1
+            igrid0 = igrid1
+            igrid1 = igrid0 + mtgrid1
+            for x in range(3):
+                key = r'%s-%d' % (self._datakeys[5+x], ipsi)
+                clog.debug("Filling datakeys: %s ..." % key)
+                sd[key] = tmpdata[igrid0:igrid1, :, x]
         return sd
 
 
@@ -162,3 +220,56 @@ class Flux3DCorrLenDigger(Flux3DTileDigger, SnapshotFieldFluxCorrLenDigger):
         self.ipsi = int(self.section[-1])
         self._fignum = '%s_%03d_corrlen' % (self.section[1], self.ipsi)
         self.kwoptions = None
+
+
+class Flux3DCoordDigger(Digger):
+    '''show flux surface for each psi.'''
+    __slots__ = ['ipsi']
+    nitems = '?'
+    itemspattern = ['^(?P<section>flux3dcoord)/Z-(?P<ipsi>\d+)$']
+    commonpattern = ['gtc/arr2']
+    post_template = 'tmpl_contourf'
+
+    def _set_fignum(self, numseed=None):
+        self.ipsi = int(self.section[-1])
+        self._fignum = '%03d' % self.ipsi
+        self.kwoptions = None
+
+    def _dig(self, kwargs):
+        '''
+        kwargs
+        ------
+        *addzeta0*: add zeta=0, use data from zeta=2pi, default True
+        '''
+        X = self.pckloader.get("%s/X-%d" % (self.group, self.ipsi))
+        Y = self.pckloader.get("%s/Y-%d" % (self.group, self.ipsi))
+        Z = self.pckloader.get("%s/Z-%d" % (self.group, self.ipsi))
+        addzeta0 = kwargs.get('addzeta0', True)
+        if self.kwoptions is None:
+            self.kwoptions = dict(
+                addzeta0=dict(widget='Checkbox',
+                              value=bool(addzeta0),
+                              description='add zeta=0:'))
+        acckwargs = dict(addzeta0=bool(addzeta0))
+        if addzeta0:
+            q = self.pckloader.get('gtc/arr2')[self.ipsi-1, 2]
+            sep = round(Z.shape[0]*(1.0-1.0/q))  # q>1
+            if sep != 0:
+                X0 = np.row_stack((X[sep:, -2:], X[:sep, -2:]))[:, -1]
+                Y0 = np.row_stack((Y[sep:, -2:], Y[:sep, -2:]))[:, -1]
+                Z0 = np.row_stack((Z[sep:, -2:], Z[:sep, -2:]))[:, -1]
+                X = np.column_stack((X0, X))
+                Y = np.column_stack((Y0, Y))
+                Z = np.column_stack((Z0, Z))
+            else:
+                X = np.column_stack((X[:, -1], X))
+                Y = np.column_stack((Y[:, -1], Y))
+                Z = np.column_stack((Z[:, -1], Z))
+        return dict(X=X, Y=Y, Z=Z,
+                    title='flux surface(ipsi=%d)' % self.ipsi), acckwargs
+
+    def _post_dig(self, results):
+        r = results
+        return dict(X=r['X'], Y=r['Y'], Z=r['Z'], plot_method='plot_surface',
+                    title=r['title'], xlabel=r'$X$',
+                    ylabel=r'$Y$', aspect='equal')
