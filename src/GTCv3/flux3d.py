f731677489b5bfb9c49d65cfe6e8a40cc2ff2cf0
shmilee <shmilee.zju@gmail.com>
2023-08-14 16:57:19 +0800

    add function flux3d.flux3d_interpolate_stack for 4-D field array

--- src/GTCv3/flux3d.py
+++ src/GTCv3/flux3d.py
@@ -173,6 +173,91 @@ def flux3d_theta_interpolation(loader, key, iM, iN, fielddir=0):
     return _fluxdata_theta_interpolation(data, q, iM, iN, fielddir)
 
 
+def flux3d_interpolate_stack(loader, saver, iM, iN, field='phi', fielddir=0,
+                             dry=False):
+    '''
+    Interpolate of flux(alpha,zeta) -> flux(theta,zeta), then stack 2-D array
+    into a 4-D array flux(time,psi,theta,zeta).
+    Save time, ipsi, theta, zeta, flux 4-D array in the *saver*.
+
+    Parameters
+    ----------
+    loader: pckloader instance
+    saver: pcksaver instance
+    iM: int, interpolation grid points in theta
+    iN: int, interpolation grid points in zeta
+    field: str, phi|apara|fluidne|densityi|temperi|densitye etc.
+    fielddir: int, 0, 1, 2 or 3, magnetic field & current direction
+    dry: bool, dry run, show array info then return
+    '''
+    import multiprocessing
+    from ..savers import is_pcksaver
+
+    if not is_pcksaver(saver):
+        dlog.error('Invalid pcksaver instance!')
+        return
+
+    _pat = re.compile('^flux3da*\d{5,7}/%s-(?P<ipsi>\d+)' % field)
+    keys = loader.refind(_pat)
+    if not keys:
+        dlog.error('Flux3d of %s: Not Found! Try phi, densityi etc.' % field)
+        return
+    _pat = re.compile('^%s/%s-(?P<ipsi>\d+)' % (keys[0].split('/')[0], field))
+    psis = [k.split('-')[1] for k in loader.refind(_pat)]
+    _pat = re.compile('^flux3da*\d{5,7}/%s-%s' % (field, psis[0]))
+    steps = [re.match(r'^flux3da*(\d{5,7})/.*', k).groups()[0]
+             for k in loader.refind(_pat)]
+    if len(steps)*len(psis) - len(keys) != 0:
+        dlog.error('Wrong keys len(steps)*len(psis)=%d*%d=%d != len(keys)=%d',
+                   (len(steps), len(psis), len(steps)*len(psis), len(keys)))
+        return
+    keys = np.array(keys).reshape((len(steps), len(psis)))
+    qs = [loader['gtc/arr2'][int(ipsi)-1, 2] for ipsi in psis]
+    tstep = loader.get('gtc/tstep')
+    time = np.array([round(int(i) * tstep, Ndigits_tstep) for i in steps])
+    ipsi = np.array([int(i) for i in psis])
+    theta = np.arange(0, iM) / (iM-1) * 2*np.pi
+    zeta = np.arange(0, iN) / (iN-1) * 2*np.pi
+    shape = (len(steps), len(psis), iM, iN)
+    size = len(steps)*len(psis)*iM*iN*4/1024/1024  # MB
+    if dry:
+        print('=> 4-D array shape: %s, size=%.1fMB' % (shape, size))
+        print('=> steps found(%d): %s ... ... %s'
+              % (len(steps), steps[:10], steps[-10:]))
+        print('=> psis found: %s' % psis)
+        return
+    f4d_arr = np.zeros(shape, dtype=np.float32)
+    ncpu = multiprocessing.cpu_count()
+
+    def worker(i, data, q):
+        dlog.info("Interpolation of step=%s, time=%f" % (steps[i], time[i]))
+        return _fluxdata_theta_interpolation(data, q, iM, iN, fielddir)[2]
+
+    for i in range(len(psis)):
+        dlog.info("Get raw-data of ipsi=%s, q=%f" % (psis[i], qs[i]))
+        rawdata = loader.get_many(*keys[:, i])
+        with multiprocessing.Pool(processes=ncpu) as pool:
+            results = [pool.apply_async(worker, (idx, rawdata[idx], qs[i]))
+                       for idx in range(len(rawdata))]
+            pool.close()
+            pool.join()
+        dlog.info("Set interpolate-data of ipsi=%s, q=%f" % (psis[i], qs[i]))
+        # tmp(time, psi=i, theta, zeta)
+        tmp = [res.get() for res in results]
+        f4d_arr[:, i, :, :] = tmp
+
+    with saver:
+        dlog.info("Writing interpolate-data(size=%.1fMB) ..." % size)
+        saver.write('/', {
+            field: f4d_arr,
+            'time': time,
+            'ipsi': ipsi,
+            'theta': theta,
+            'zeta': zeta,
+        })
+    dlog.info('Done.')
+
+
 class Flux3DAlphaTileDigger(Flux3DAlphaDigger, SnapshotFieldFluxAlphaTileDigger):
     '''Tiled phi(alpha,zeta), a_para, etc. on every flux surface.'''
     __slots__ = []
@@ -208,7 +293,7 @@ class Flux3DAlphaCorrLenDigger(Flux3DAlphaTileDigger, SnapshotFieldFluxAlphaCorr
         self.kwoptions = None
 
 
-class Flux3DAlphaKthetaOmegaDigger(Flux3DAlphaDigger):  #TODO
+class Flux3DAlphaKthetaOmegaDigger(Flux3DAlphaDigger):  # TODO
     '''phi(ktheta,omega), a_para etc. on every flux surface.'''
     __slots__ = []
     itemspattern = [
@@ -221,4 +306,3 @@ class Flux3DAlphaKthetaOmegaDigger(Flux3DAlphaDigger):  #TODO
             self._group = self.section[0].replace('flux3da', 'flux3d')
         else:
             self._group = self.section[0]
-
