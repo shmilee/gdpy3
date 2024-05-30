45cb111bd3b4cb75a201a4b5a7c9111b0559a243
shmilee <shmilee.zju@gmail.com>
2024-05-30 18:41:00 +0800

    add GTCv3, Numerical_r_psi for numerical equilibrium

--- src/GTCv3/misc.py
+++ src/GTCv3/misc.py
@@ -2,13 +2,14 @@
 
 # Copyright (c) 2020-2021 shmilee
 
-import numpy
+import numpy as np
+from .. import tools
 
-__all__ = ['Trans_r_psi']
+__all__ = ['Analytic_r_psi', 'Numerical_r_psi']
 
 
-class Trans_r_psi(object):
-    ''' r, r/a <-> psi, psi/psiw'''
+class Analytic_r_psi(object):
+    ''' r, r/a <-> psi, psi/psiw for analytic equilibrium'''
     __slots__ = ['q', 'psiw', 'a']
 
     def __init__(self, q=(0.85, 1.04, 1.0), psiw=0.0381):
@@ -23,7 +24,7 @@ class Trans_r_psi(object):
         '''
         q1, q2, q3 = self.q
         r2 = 2*(q1*npsi+q2*npsi*npsi/2.0+q3*npsi*npsi*npsi/3.0)*self.psiw
-        return numpy.sqrt(r2)
+        return np.sqrt(r2)
 
     def nrnpsi(self, npsi):
         '''
@@ -39,7 +40,7 @@ class Trans_r_psi(object):
         '''
         q1, q2, q3 = self.q
         # q1*psi+q2*psi*psi/2.0+q3*psi*psi*psi/3.0 - r**2 / (2*psiw) = 0
-        roots = numpy.roots([q3/3.0, q2/2.0, q1, - r**2 / (2*self.psiw)])
+        roots = np.roots([q3/3.0, q2/2.0, q1, - r**2 / (2*self.psiw)])
         for npsi in roots:
             if npsi.imag == 0 and npsi.real >= 0:
                 return npsi.real
@@ -73,13 +74,67 @@ class Trans_r_psi(object):
         Input q(npsi) 3 points and a
         Return (q1, q2, q3), psiw
         '''
-        A = numpy.mat([
+        A = np.mat([
             [1.0, q_p1[0], q_p1[0]**2.0],
             [1.0, q_p2[0], q_p2[0]**2.0],
             [1.0, q_p3[0], q_p3[0]**2.0],
         ])
-        b = numpy.mat([q_p1[1], q_p2[1], q_p3[1]]).T
-        q = numpy.linalg.solve(A, b)
+        b = np.mat([q_p1[1], q_p2[1], q_p3[1]]).T
+        q = np.linalg.solve(A, b)
         q1, q2, q3 = q[0, 0], q[1, 0], q[2, 0]
         psiw = a**2.0/2.0/(q1+q2/2.0+q3/3.0)
         return (q1, q2, q3), psiw
+
+
+class Numerical_r_psi(object):
+    ''' r, r/a <-> psi, psi/psiw for numerical equilibrium'''
+    __slots__ = ['r', 'a', 'psi', 'psiw']
+
+    def __init__(self, r=None, psi=None, gdp=None):
+        if r and psi:
+            self.r, self.psi = np.array(r), np.array(psi)
+        elif gdp:
+            a, b, c = gdp.dig('equilibrium/r(psi)', post=False)
+            self.r, self.psi = b['Y'], b['X']
+        else:
+            raise ValueError('Input r, psi together or just a GTC processor!')
+        if self.r.size != self.psi.size:
+            raise ValueError('Array r.size != psi.size !')
+        self.a = self.r[-1]
+        self.psiw = self.psi[-1]
+
+    def __fit_coeffs(self, X, Y, deg):
+        c, fy = tools.line_fit(X, Y, deg)
+        std1 = np.std(Y - fy)
+        std2 = np.std((Y[1:] - fy[1:]) / Y[1:])  # skip [0] zero
+        print('fit residual error: %f, relative: %.3f%%' % (std1, std2*100))
+        return c[0]
+
+    def rpsi_fit_coeffs(self, x='r', deg=10):
+        '''
+        x: str, 'r' or 'psi'
+        Return polynomial coefficients, highest power first.
+        '''
+        X = self.psi if x == 'psi' else self.r
+        Y = self.r if x == 'psi' else self.psi
+        return self.__fit_coeffs(X, Y, deg)
+
+    def nrnpsi_fit_coeffs(self, x='nr', deg=10):
+        '''
+        x: str, 'nr'(r/a) or 'npsi'(psi/psiw)
+        Return polynomial coefficients, highest power first.
+        '''
+        X = self.psi/self.psiw if x == 'npsi' else self.r/self.a
+        Y = self.r/self.a if x == 'npsi' else self.psi/self.psiw
+        return self.__fit_coeffs(X, Y, deg)
+
+    @staticmethod
+    def string_polynomial(coeffs1d, x='r'):
+        ''' x: str symbol'''
+        p1d = ''
+        for c in coeffs1d:
+            if p1d:
+                p1d = '(%s*%s %+.8e)' % (p1d, x, c)
+            else:
+                p1d = '%.8e' % c
+        return p1d
